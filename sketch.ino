26317674df71be3c4e0833332acf1716a494ed8f
Eric Wätke <eric.waetke@fh-potsdam.de>
2024-06-09 14:31:45 +0200

    Added Banks and Motor

--- sketch.ino
+++ sketch.ino
@@ -1,7 +1,7 @@
 // #include "MIDIUSB.h"
 // #include "PitchToNote.h"
 #include <Control_Surface.h>  // Include the library
-
+ 
 USBMIDI_Interface midi;  // Instantiate a MIDI Interface to use
 CCPotentiometer pot0 { A0, MIDI_CC::General_Purpose_Controller_1 };
 CCPotentiometer pot1 { A1, MIDI_CC::General_Purpose_Controller_2 };
@@ -9,11 +9,25 @@ CCPotentiometer pot2 { A2, MIDI_CC::General_Purpose_Controller_3 };
 CCPotentiometer pot3 { A3, MIDI_CC::General_Purpose_Controller_4 };
 CCPotentiometer pot4 { A4, MIDI_CC::General_Purpose_Controller_5 };
 
+Bank<4> bank(1); // 3 banks with an offset of 4 tracks per bank (a = 4)
+ 
+Bankable::CCPotentiometer fader[] {
+    {bank, A5, 1}, // base address 1 (b = 1)
+};
+
+#define IN1 7       // Motor 1 - Forward
+#define IN2 8       // Motor 1 - Reverse
+#define ENABLE 6    // Enable pin for both motors (PWM enabled)
+#define FADER_POT A5
+
+void setPWMPrescaler(uint8_t prescaler) {
+  TCCR1B = (TCCR1B & B11111000) | prescaler;
+}
+
+
 // The setup function runs once when you press reset or power the board
 void setup() {
   Serial.begin(9600);
-
-  // pinMode(7, INPUT);
 
   // Initialize pins 10-13 as outputs
   pinMode(9, OUTPUT);
@@ -27,6 +41,12 @@ void setup() {
   pinMode(5, INPUT_PULLUP);
   boot();
   Control_Surface.begin();
+
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(ENABLE, OUTPUT);
+  // Set initial PWM frequency for pin 9 (Timer 1)
+  setPWMPrescaler(1);
 }
 
 int lit = 0;
@@ -35,15 +55,25 @@ int lit = 0;
 void loop() {
   Control_Surface.loop();
 
+  Serial.println(bank.getSelection());
+
   // Check if the button is pressed
   if (digitalRead(2) == LOW) {
     lit = 12;
+    bank.select(0);
+    moveTo(fader[0].getValue());
   } else if (digitalRead(3) == LOW) {
     lit = 11;
+    bank.select(1);
+    moveTo(fader[1].getValue());
   } else if (digitalRead(4) == LOW) {
     lit = 10;
+    bank.select(2);
+    moveTo(fader[2].getValue());
   } else if (digitalRead(5) == LOW) {
     lit = 9;
+    bank.select(3);
+    moveTo(fader[3].getValue());
   }
 
   digitalWrite(9, LOW);
@@ -58,7 +88,7 @@ void loop() {
 void boot() {
   int speed = 100;
 
-  for (int iter = 0; iter < 4; iter++) {
+  for (int iter = 0; iter <= 2; iter++) {
     for (int step = 1; step <= 4; step++) {
       selected_led(step);
       delay(speed);
@@ -101,4 +131,46 @@ void selected_led(int selected) {
       digitalWrite(12, LOW);
       break;
   }
+}
+
+
+// Function to move the fader to a specified position
+void moveTo(int targetPosition) {
+  int currentPosition = analogRead(FADER_POT);
+
+  // Move the fader until it reaches the target position
+  while (abs(targetPosition - currentPosition) > 5) { // Allowing some tolerance
+    int distance = abs(targetPosition - currentPosition);
+    int speed = map(distance, 0, 1023, 170, 220); // Proportional control
+    // int speed = 170;
+
+    if (targetPosition > currentPosition) {
+      Serial.println("Moving Up");
+      // Move fader up
+      digitalWrite(IN1, HIGH);
+      digitalWrite(IN2, LOW);
+    } else {
+      Serial.println("Moving Down");
+      // Move fader down
+      digitalWrite(IN1, LOW);
+      digitalWrite(IN2, HIGH);
+    }
+
+    // Set speed (adjust value as needed)
+    analogWrite(ENABLE, speed); // Use the dynamically calculated speed
+
+    // Read current position again
+    currentPosition = analogRead(FADER_POT);
+
+    // Debug output
+    Serial.print("Current Position: ");
+    Serial.println(currentPosition);
+    Serial.print("Speed: ");
+    Serial.println(speed);
+  }
+
+  // Stop the motor once the position is reached
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  analogWrite(ENABLE, 0);
 }
