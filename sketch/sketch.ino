ce1f6053fa48ccf80d7d8408f060cce2fcf17600
Eric Wätke <eric.waetke@fh-potsdam.de>
2024-06-10 11:29:36 +0200

    Renamed all Vars

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -46,21 +46,22 @@ void setup() {
   Serial.begin(9600);
 
   // Initialize pins 10-13 as outputs
-  pinMode(9, OUTPUT);
-  pinMode(10, OUTPUT);
-  pinMode(11, OUTPUT);
-  pinMode(12, OUTPUT);
+  pinMode(LED0, OUTPUT);
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
 
-  pinMode(2, INPUT_PULLUP);
-  pinMode(3, INPUT_PULLUP);
-  pinMode(4, INPUT_PULLUP);
-  pinMode(5, INPUT_PULLUP);
+  pinMode(BUTTON0, INPUT_PULLUP);
+  pinMode(BUTTON1, INPUT_PULLUP);
+  pinMode(BUTTON2, INPUT_PULLUP);
+  pinMode(BUTTON3, INPUT_PULLUP);
   boot();
   Control_Surface.begin();
 
-  pinMode(IN1, OUTPUT);
-  pinMode(IN2, OUTPUT);
-  pinMode(ENABLE, OUTPUT);
+  pinMode(FADER_FORWARD, OUTPUT);
+  pinMode(FADER_REVERSE, OUTPUT);
+  pinMode(FADER_SPEED, OUTPUT);
+
   // Set initial PWM frequency for pin 9 (Timer 1)
   setPWMPrescaler(1);
 }
@@ -74,32 +75,32 @@ void loop() {
   Serial.println(fader[bank.getSelection()].getRawValue());
 
   // Check if the button is pressed
-  if (digitalRead(2) == LOW) {
+  if (digitalRead(BUTTON0) == LOW) {
     lit = 12;
     bank.select(0);
     // moveTo(fader[0].getValue());
     Serial.println(fader[0].getValue());
-  } else if (digitalRead(3) == LOW) {
+  } else if (digitalRead(BUTTON1) == LOW) {
     lit = 11;
     bank.select(1);
     // moveTo(fader[1].getValue());
     Serial.println(fader[1].getValue());
-  } else if (digitalRead(4) == LOW) {
+  } else if (digitalRead(BUTTON2) == LOW) {
     lit = 10;
     bank.select(2);
     // moveTo(fader[2].getValue());
     Serial.println(fader[2].getValue());
-  } else if (digitalRead(5) == LOW) {
+  } else if (digitalRead(BUTTON3) == LOW) {
     lit = 9;
     bank.select(3);
     // moveTo(fader[3].getValue());
     Serial.println(fader[3].getValue());
   }
 
-  digitalWrite(9, LOW);
-  digitalWrite(10, LOW);
-  digitalWrite(11, LOW);
-  digitalWrite(12, LOW);
+  digitalWrite(LED0, LOW);
+  digitalWrite(LED1, LOW);
+  digitalWrite(LED2, LOW);
+  digitalWrite(LED3, LOW);
 
   // Turn on the LED at the current position
   digitalWrite(lit, HIGH);
@@ -116,10 +117,10 @@ void boot() {
   }
 
   // Turn off all LEDs
-  digitalWrite(9, LOW);
-  digitalWrite(10, LOW);
-  digitalWrite(11, LOW);
-  digitalWrite(12, LOW);
+  digitalWrite(LED0, LOW);
+  digitalWrite(LED1, LOW);
+  digitalWrite(LED2, LOW);
+  digitalWrite(LED3, LOW);
 
   lit = 12;
 }
@@ -127,70 +128,55 @@ void boot() {
 void selected_led(int selected) {
   switch (selected) {
     case 1:
-      digitalWrite(9, LOW);
-      digitalWrite(10, LOW);
-      digitalWrite(11, LOW);
-      digitalWrite(12, HIGH);
+      digitalWrite(LED0, LOW);
+      digitalWrite(LED1, LOW);
+      digitalWrite(LED2, LOW);
+      digitalWrite(LED3, HIGH);
       break;
     case 2:
-      digitalWrite(9, LOW);
-      digitalWrite(10, LOW);
-      digitalWrite(11, HIGH);
-      digitalWrite(12, LOW);
+      digitalWrite(LED0, LOW);
+      digitalWrite(LED1, LOW);
+      digitalWrite(LED2, HIGH);
+      digitalWrite(LED3, LOW);
       break;
     case 3:
-      digitalWrite(9, LOW);
-      digitalWrite(10, HIGH);
-      digitalWrite(11, LOW);
-      digitalWrite(12, LOW);
+      digitalWrite(LED0, LOW);
+      digitalWrite(LED1, HIGH);
+      digitalWrite(LED2, LOW);
+      digitalWrite(LED3, LOW);
       break;
     case 4:
-      digitalWrite(9, HIGH);
-      digitalWrite(10, LOW);
-      digitalWrite(11, LOW);
-      digitalWrite(12, LOW);
+      digitalWrite(LED0, HIGH);
+      digitalWrite(LED1, LOW);
+      digitalWrite(LED2, LOW);
+      digitalWrite(LED3, LOW);
       break;
   }
 }
 
-
 // Function to move the fader to a specified position
 void moveTo(int targetPosition) {
-  int currentPosition = analogRead(FADER_POT);
-
   // Move the fader until it reaches the target position
   while (abs(targetPosition - currentPosition) > 5) { // Allowing some tolerance
     int distance = abs(targetPosition - currentPosition);
     int speed = map(distance, 0, 1023, 170, 220); // Proportional control
-    // int speed = 170;
 
     if (targetPosition > currentPosition) {
       Serial.println("Moving Up");
       // Move fader up
-      digitalWrite(IN1, HIGH);
-      digitalWrite(IN2, LOW);
+      digitalWrite(FADER_FORWARD, HIGH);
+      digitalWrite(FADER_REVERSE, LOW);
     } else {
       Serial.println("Moving Down");
       // Move fader down
-      digitalWrite(IN1, LOW);
-      digitalWrite(IN2, HIGH);
+      digitalWrite(FADER_FORWARD, LOW);
+      digitalWrite(FADER_REVERSE, HIGH);
     }
-
-    // Set speed (adjust value as needed)
-    analogWrite(ENABLE, speed); // Use the dynamically calculated speed
-
-    // Read current position again
-    currentPosition = analogRead(FADER_POT);
-
-    // Debug output
-    Serial.print("Current Position: ");
-    Serial.println(currentPosition);
-    Serial.print("Speed: ");
-    Serial.println(speed);
+    analogWrite(ENABLE, speed);
   }
 
   // Stop the motor once the position is reached
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, LOW);
+  digitalWrite(FADER_FORWARD, LOW);
+  digitalWrite(FADER_REVERSE, LOW);
   analogWrite(ENABLE, 0);
 }
